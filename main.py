20e8bac6707d59b71c3b0a26b01fb66db1606cda
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-06-10 23:26:38 +0400

    Scrapping Events from url

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import selenium
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 
 url = "https://www.python.org/"
-driver = webdriver.Chrome()
+
+# initialize the web driver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+# Open the python.org
+
+driver.get(url)
+events_times = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time")
+events_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget li a")
+
+events = {}
+
+for n in range(len(events_times)):
+    events[n] = {
+        "time": events_times[n].text,
+        "name": events_names[n].text
+    }
+
+print(events)
